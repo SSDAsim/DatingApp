c72027a825a36a59bd8344193ed9349dc42ab623
SSDAsim <asim.saeed@halimsol.com>
2026-01-15 18:56:56 +0500

    End of Section 04

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,12 @@
+using System.Text;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens.Experimental;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,10 +22,27 @@ builder.Services.AddCors();
 // register Token Service
 builder.Services.AddScoped<ITokenService, TokenService>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+       var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Token key not found - Program.cs"); 
+       options.TokenValidationParameters = new TokenValidationParameters
+       {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+       };
+    });
+
 var app = builder.Build();
 
+// Middlewares - the order of the middleware matters
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+app.UseAuthentication(); // answers who is the user 
+app.UseAuthorization(); // answers is user allowed to do the task
 
 app.MapControllers();
 
